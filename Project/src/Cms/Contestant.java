2bd8fa5d1adab566fee1974aa7e64d0067299a50
mynkhe150888 <73680420+minhnthe150888@users.noreply.github.com>
2020-11-02 14:48:07 +0700

    Contestant class
    Con

---
+++ Project/src/Cms/Contestant.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class Contestant {
+    String name;
+    String id;
+    String email; 
+    String mobilephone; 
+    Unsigned int 
+}
